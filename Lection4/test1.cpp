ff500fa821fe7bd9f487ca0f200489861c6bac10
Lev Kazakov <PrOrAb651@gmail.com>
2021-08-12 14:02:58 +0300

    examples of working ternar operator with different types and same types

--- Lection4/test1.cpp
+++ Lection4/test1.cpp
@@ -2,7 +2,13 @@
 #include <string>
 
 int main() {
+    std::string s = "abc";
+    std::string ss = "abcde";
+    std::cout << (2 < 3 ? s : ss).size();
+
     int x = 0;
-    double y = 1;
+    double y = 1.0;
     std::cout << (2 < 3 ? x : y);
+
+    2 < 3 ? std::cout << x : std::cout << s;
 }
