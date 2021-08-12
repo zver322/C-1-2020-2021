460fb9667ede0b2beccc36f8738009f6f1254ce0
Lev Kazakov <PrOrAb651@gmail.com>
2021-08-12 14:28:03 +0300

    example of working lvalue and rvalue

--- Lection4/test1.cpp
+++ Lection4/test1.cpp
@@ -4,11 +4,18 @@
 int main() {
     std::string s = "abc";
     std::string ss = "abcde";
-    std::cout << (2 < 3 ? s : ss).size();
+    std::cout << (2 < 3 ? s : ss).size() << "\n";
 
     int x = 0;
     double y = 1.0;
-    std::cout << (2 < 3 ? x : y);
+    std::cout << (2 < 3 ? x : y) << "\n";
 
     2 < 3 ? std::cout << x : std::cout << s;
+
+    ++x = x++;
+    std::cout << "\n" << x << "\n";
+
+    (2 < 3 ? ++x : x++);
+
+    
 }
