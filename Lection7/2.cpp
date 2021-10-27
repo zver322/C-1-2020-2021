b2a88893bfd4104666d6126c263890b7ec12f31d
zver322 <PrOrAb651@gmail.com>
2021-10-27 15:23:55 +0300

    more info about classes

--- Lection7/2.cpp
+++ Lection7/2.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 
-struct D {
+class C {
 private:
     int x = 0;
     std::string s;
     double d = 0.0; // Размер типа будет равен сумме размеров его полей 
-public:
-    void add_and_print(int a) {
-        std::cout << x + a << std::endl;
-    }
-};
-
-class C {
-    private:
-    int x = 0;
-    std::string s;
-    double d = 0.0; // Размер типа будет равен сумме размеров его полей 
-    void add_and_print(int a) {
-        std::cout << x + a << std::endl;
-    }
+    void add_and_print(int);
 public:
     void add_and_print(double a) {
         std::cout << x + a << std::endl;
     }
 };
 
+void C::add_and_print(int x) {
+    //std::cout << C::x + x << std::endl;
+    std::cout << this->x + x << std::endl; // Две идентичные по смыслу записи
+}
+
 int main() {
     C c;
-    D d;
     std::cout << sizeof(c) << std::endl; // Занимает 1 байт 
-    std::cout << sizeof(d) << std::endl; // Занимает 48 байт, равен сумме полей. Было произведено округление.
-    // c.x = 0; - так нельзя. Данное поле приватн 
-    c.add_and_print(25);
+    c.add_and_print(25.0);
     return 0;
 }
